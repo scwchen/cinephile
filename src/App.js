f07b72ac1d8136e1b493d12926bf0f965c9c69fd
Steven Chen <steven.chen.121@gmail.com>
2021-12-30 11:36:07 -0500

    Added some base SCSS and outline of app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './sass/App.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cinephile</h1>
+      {/* 
+      Pages
+      - Rules (How to Play)
+        - Filmography (like Categories)
+        - Head On (like Heads up)
+        - Movie, Actor (kind of like Telestrations bouncing between movies and actors)
+        - Take, Six (kind of like uno in that you are trying to get rid of your cards but have to do so by connecting them to the card played) this is had
+        - Six Degrees (connecting the two cards in six degrees or less)
+      - Game itself
+        - just flipping cards and giving option to flip based on game mode and discard/skip if too hard.
+        - for a time limit you can add a timer if you wish. 
+      - Deck (this will change based on tmdb popular movies/actors);
+        - movies 
+        - actors 
+      - trending 
+      https://api.themoviedb.org/3/trending/person/week?api_key={api_key}
+        - web scrape and add the remixes:
+        https://www.cinephilegame.com/remixes
+      */}
     </div>
   );
 }
